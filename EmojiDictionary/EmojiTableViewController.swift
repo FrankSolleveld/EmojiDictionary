ecd61a77f6d4ce28196a00a4dadcbb05ec960b28
Frank <fsolleveld@outlook.com>
2019-10-29 14:12:21 +0100

    editing enabled

--- EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiTableViewController.swift
@@ -15,20 +15,14 @@ class EmojiTableViewController: UITableViewController {
         Emoji(symbol: "🍟", name: "French fries", description: "Freshly baked French fries.", usage: "When you don't feel like eating healthy, this is the best choice."),
         Emoji(symbol: "😡", name: "Mad af", description: "This guy is mad because his code won't compile.", usage: "When you have spend several hours trying to fix a specifix bug and it still does not resolve the issue."),
         Emoji(symbol: "😬", name: "Cringy guy", description: "This face is cringing hard.", usage: "When you notice soemthing is really cringe you just wanna fade out."),
-        Emoji(symbol: "🤯", name: "Exploded", description: "This person is really surprised and therebor his head is blown up.", usage: "When something so unexpectedly happend your head explodes."),
+        Emoji(symbol: "🤯", name: "Exploded", description: "This person is really surprised and therefor his head is blown up.", usage: "When something so unexpectedly happend your head explodes."),
         Emoji(symbol: "👺", name: "Satan", description: "Meet Satan", usage: "When you want to show that you are a Satan worshipper."),
     ]   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tableView.cellLayoutMarginsFollowReadableWidth = true
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
@@ -51,52 +45,29 @@ class EmojiTableViewController: UITableViewController {
         let emoji = emojis[indexPath.row]
         cell.textLabel?.text = "\(emoji.symbol) - \(emoji.name)"
         cell.detailTextLabel?.text = emoji.description
+        cell.showsReorderControl = true
         return cell
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        print("\(emoji.symbol) + \(indexPath.row)")
+    }
     
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedEmoji = emojis.remove(at: sourceIndexPath.row)
+        emojis.insert(movedEmoji, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
     
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
     
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
+    @IBAction func editButtontapped(_ sender: UIBarButtonItem) {
+        let tableViewEditingMode = tableView.isEditing
+        tableView.setEditing(!tableViewEditingMode, animated: true)
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
